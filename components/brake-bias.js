c5eeee4ec64d86c1d66d2d177113362e280752bf
Caio Lelis Penido <caiolelis337@hotmail.com>
2023-03-29 11:06:44 -0300

    Mudei components na página de controle

--- components/brake-bias.js
+++ components/brake-bias.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 
 class BrakeBias extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props)
-    }
-
-    render(){
-        <div>
-            <h1>BRAKE BIAS:</h1>
-            <h2>{this.props.value}</h2>
-        </div>
-    }
+  render() {
+    return (
+      <div className="default-container bordered-title-container">
+        <div className="component-title bordered-title-text">BRAKE BIAS</div>
+        <h2>{this.props.value} %</h2>
+      </div>
+    );
+  }
 }
 
 export default BrakeBias;
--- components/ecu.js
+++ components/ecu.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 
 class ECU extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props)
-    }
-
-    render(){
-        <div>
-            <h1>ECU:</h1>
-            <h2>{this.props.value}</h2>
-        </div>
-    }
+  render() {
+    return (
+      <div className="default-container bordered-title-container">
+        <div className="component-title bordered-title-text">ECU FLAG</div>
+        <h2>{this.props.value}</h2>
+      </div>
+    );
+  }
 }
 
 export default ECU;
